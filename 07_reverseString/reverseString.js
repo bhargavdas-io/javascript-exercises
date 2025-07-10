fa86348f466f8915122e6d01ebf3ffe49ea03822
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-10 14:10:03 +0000

    string reversal

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let text = string;
 
+  const reversed = text.split("").reverse().join("");
+
+  return reversed;
 };
 
 // Do not edit below this line
