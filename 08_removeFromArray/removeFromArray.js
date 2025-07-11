c082b07f6340149ba8f771fea600bf57b9eb9e8f
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-11 08:11:22 +0000

    Remove multiple items from array solution using filter()

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const myArr = array;
+  return myArr.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
