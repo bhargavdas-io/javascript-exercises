ba6efa8e87bc2e538fae38e8a405b8cadf66b50d
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-12 15:46:50 +0000

    sumALL solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 != "number" ||
+    typeof num2 != "number" ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let integers = [];
+  let totalsum = 0;
+  let start = Math.min(num1, num2);
+  let end = Math.max(num1, num2);
+
+  for (let i = start; i <= end; i++) {
+    integers.push(i);
+  }
+
+  for (let j = 0; j < integers.length; j++) {
+    totalsum += integers[j];
+  }
+
+  return totalsum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
