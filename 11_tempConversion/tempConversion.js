f178f95ffba53ebd163831c0e2bd05a758e7841b
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-13 15:35:35 +0000

    Temp Conversion solution and end of loops, array module

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempf) {
+  let inCelcius = ((tempf - 32) * 5) / 9;
+  const roundedresult = parseFloat(inCelcius.toFixed(1));
+
+  return roundedresult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempc) {
+  let inFahrenheit = (tempc * 9) / 5 + 32;
+  const roundedresult = parseFloat(inFahrenheit.toFixed(1));
+
+  return roundedresult;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
