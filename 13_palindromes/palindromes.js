79d174b39baee14849d47c50dadd2bca236dafb3
bhargavdas-io <bhargavdas77@hotmail.com>
2025-08-13 15:07:16 +0000

    Palindrome solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleanstring = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversedstring = cleanstring.split("").reverse().join("");
+  if (reversedstring === cleanstring) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
