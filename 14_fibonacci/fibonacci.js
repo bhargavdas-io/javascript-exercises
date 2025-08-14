02a610a8e7b8ab2ee327f88fbcbcb5676a740494
bhargavdas-io <bhargavdas77@hotmail.com>
2025-08-14 16:18:48 +0000

    fibonacci solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num === 0 || num === "0") {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  }
+  let arr = [1, 1];
 
+  for (let i = 2; i < num; i++) {
+    let nxtnum = arr[i - 1] + arr[i - 2];
+    arr.push(nxtnum);
+  }
+
+  return arr[num - 1];
 };
 
 // Do not edit below this line
